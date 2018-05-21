b3510bce11ca45a71f182765012d3aaf4994e582
Kalen Hammann <kalen7@gmail.com>
2018-05-21 17:34:52 -0400

    Added first link, to 'Home'(App component) from Stack component

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Stack extends Component {
     render() {
         return (
             <div>
+                <Link to='/'>Home</Link>
                 <h3>example title</h3>
             </div>
         )
