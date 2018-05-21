2a8b8b2d7d45a846944324c6640f540c7236df6f
Kalen Hammann <kalen7@gmail.com>
2018-05-21 17:22:36 -0400

    Added Stack component and new route to it

---
+++ src/components/Stack.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Stack extends Component {
+    render() {
+        return (
+            <div>
+                <h3>example title</h3>
+            </div>
+        )
+    }
+}
+
+export default Stack;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './components/App';
+import Stack from './components/Stack';
 
 ReactDom.render(
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={App} />
+            <Route path='/stack' component={Stack} />
         </Switch>
     </BrowserRouter>, 
     document.getElementById('root')
